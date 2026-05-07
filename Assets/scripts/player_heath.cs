3e3911b82b17e3952f1a17ff63cfe7c1655f5fa0
martiņš <martins.mikelsons7@gmail.com>
2026-05-07 20:35:56 +0300

    sviras un vinjete

--- Assets/scripts/player_heath.cs
+++ Assets/scripts/player_heath.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -47,7 +48,15 @@ public class Player_Heath : MonoBehaviour
         //Debug.Log("Before taking damage: " + health+" "+damage); // Check the health value before modification
         health =- damage;
         //Debug.Log("After taking damage: " + health+" "+damage);  // Check the health value after modification
+        if (health <= 0)
+        {
+            DIE();
+        }
 
         UpdateVisualHealth();
+    }
+    void DIE()
+    {
+
     }
 }
