ded5afb86623a202c96691a4bcda8abb95396674
martiņš <martins.mikelsons7@gmail.com>
2026-05-16 10:55:23 +0300

    reverted texture coruption

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,35 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Player_Heath : MonoBehaviour
+public class PlayerHealth : MonoBehaviour , IDamageable
 {
     public float MaxHealth = 100f;
     public float health;
     public GameObject healthbar;
-     Slider healthSlider;
-
-    private void Awake()
-    {
-        
-    }
+    Slider healthSlider;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
         health = MaxHealth;
-        
+
         if (healthbar != null)
         {
             healthSlider = healthbar.GetComponent<Slider>();
             healthSlider.maxValue = MaxHealth;
             healthSlider.value = health;
-           
+
         }
         else
         {
-            Debug.LogError("Health bar is not assigned in the Inspector!"+healthbar.GetComponent<Slider>()+"!!!"+healthbar.name+""+healthSlider.name);
+            Debug.LogError("Health bar is not assigned in the Inspector!" + healthbar.GetComponent<Slider>() + "!!!" + healthbar.name + "" + healthSlider.name);
         }
     }
-
     public void UpdateVisualHealth()
     {
         if (healthSlider != null)
@@ -38,16 +33,21 @@ public class Player_Heath : MonoBehaviour
         }
         else
         {
-          //  Debug.LogError("Health slider is null");
+            //  Debug.LogError("Health slider is null");
         }
     }
-
-    public void Take(float damage)
-    {
+    
+    void Death() { 
+        Time.timeScale = 0.0f;
+    }
+    public void TakeDamage(float damage) {
         //Debug.Log("Before taking damage: " + health+" "+damage); // Check the health value before modification
-        health =- damage;
+        health = -damage;
         //Debug.Log("After taking damage: " + health+" "+damage);  // Check the health value after modification
 
         UpdateVisualHealth();
+        if (health < 1) {
+            Death();
+        }
     }
 }
