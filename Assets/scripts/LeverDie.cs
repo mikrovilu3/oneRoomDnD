4d971a9555822b2d5250d5f1d3c049e5a5b05ebd
martiņš <martins.mikelsons7@gmail.com>
2026-06-03 16:40:28 +0300

    the make ing of trap

--- Assets/scripts/LeverDie.cs
+++ Assets/scripts/LeverDie.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class LeverDie : MonoBehaviour
 {
-    HingeJoint joint;
+    public HingeJoint joint;
+    public GameObject arrow;
+    public Vector3 arrowDirection;
+    public float arrowSpread;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,8 +15,9 @@ public class LeverDie : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (joint==null || joint.angle >= 100)
+        if (joint==null || joint.angle <= 0)
         {
+            Instantiate(arrow,new Vector3(0,0,0),);
 
         }
     }
