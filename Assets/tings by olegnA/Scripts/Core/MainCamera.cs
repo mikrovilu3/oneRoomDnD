afe9ad1e5e65f2a02346206e70c3c5fbac100e6a
vesels cilvēks <kovalevskisangelo53@gmail.com>
2026-06-04 11:47:21 +0300

    portal runner with portal 2 reference maybe somewher in some folder in some file witha script in a single line of a single number in its bianary code in its first 1 followed by the very last p in that files binary codes last 0 and the number before that 0

---
+++ Assets/tings by olegnA/Scripts/Core/MainCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    private Portal[] portals;
+
+    void Start()
+    {
+        portals = FindObjectsByType<Portal>(
+            FindObjectsSortMode.None);
+    }
+
+    void LateUpdate()
+    {
+        if (portals == null)
+            return;
+
+        for (int i = 0; i < portals.Length; i++)
+        {
+            if (portals[i] != null)
+                portals[i].PrePortalRender();
+        }
+
+        for (int i = 0; i < portals.Length; i++)
+        {
+            if (portals[i] != null)
+                portals[i].Render();
+        }
+
+        for (int i = 0; i < portals.Length; i++)
+        {
+            if (portals[i] != null)
+                portals[i].PostPortalRender();
+        }
+    }
+}
---
+++ Assets/tings by olegnA/Scripts/Core/Portal.cs
@@ -0,0 +1,303 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Unity.XR.CoreUtils;
+
+public class Portal : MonoBehaviour
+{
+    [Header("Portal Settings")]
+    public Portal linkedPortal;
+    public MeshRenderer screen;
+    public int recursionLimit = 2;
+
+    [Header("XR")]
+    public XROrigin xrOrigin;
+    public Camera xrCamera;
+
+    [Header("Clipping")]
+    public float nearClipOffset = 0.05f;
+    public float nearClipLimit = 0.2f;
+
+    RenderTexture viewTexture;
+    Camera portalCam;
+
+    List<PortalTraveller> trackedTravellers =
+        new List<PortalTraveller>();
+
+    MeshFilter screenMeshFilter;
+
+    void Awake()
+    {
+        portalCam = GetComponentInChildren<Camera>();
+
+        if (xrCamera == null)
+        {
+            xrCamera = FindFirstObjectByType<Camera>();
+        }
+
+        portalCam.enabled = false;
+
+        screenMeshFilter =
+            screen.GetComponent<MeshFilter>();
+
+        screen.material.SetInt("displayMask", 1);
+
+        CreateViewTexture();
+    }
+
+    void LateUpdate()
+    {
+        HandleTravellers();
+    }
+
+    void CreateViewTexture()
+    {
+        int width = Mathf.Max(Screen.width, 2048);
+        int height = Mathf.Max(Screen.height, 2048);
+
+        if (viewTexture != null)
+        {
+            if (viewTexture.width == width &&
+                viewTexture.height == height)
+                return;
+
+            viewTexture.Release();
+        }
+
+        viewTexture = new RenderTexture(
+            width,
+            height,
+            24,
+            RenderTextureFormat.Default
+        );
+
+        portalCam.targetTexture = viewTexture;
+
+        linkedPortal.screen.material.SetTexture(
+            "_MainTex",
+            viewTexture
+        );
+    }
+
+    public void Render()
+    {
+        if (xrCamera == null)
+            return;
+
+        if (!CameraUtility.VisibleFromCamera(
+                linkedPortal.screen,
+                xrCamera))
+            return;
+
+        Matrix4x4 m =
+            transform.localToWorldMatrix *
+            linkedPortal.transform.worldToLocalMatrix *
+            xrCamera.transform.localToWorldMatrix;
+
+        portalCam.transform.SetPositionAndRotation(
+            m.GetColumn(3),
+            m.rotation
+        );
+
+        portalCam.fieldOfView =
+            xrCamera.fieldOfView;
+
+        portalCam.aspect =
+            xrCamera.aspect;
+
+        SetNearClipPlane();
+
+        screen.shadowCastingMode =
+            UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+
+        linkedPortal.screen.material.SetInt(
+            "displayMask",
+            0
+        );
+
+        portalCam.Render();
+
+        linkedPortal.screen.material.SetInt(
+            "displayMask",
+            1
+        );
+
+        screen.shadowCastingMode =
+            UnityEngine.Rendering.ShadowCastingMode.On;
+    }
+
+    void SetNearClipPlane()
+    {
+        Transform clipPlane = transform;
+
+        int dot = System.Math.Sign(
+            Vector3.Dot(
+                clipPlane.forward,
+                transform.position -
+                portalCam.transform.position
+            )
+        );
+
+        Vector3 camSpacePos =
+            portalCam.worldToCameraMatrix
+            .MultiplyPoint(clipPlane.position);
+
+        Vector3 camSpaceNormal =
+            portalCam.worldToCameraMatrix
+            .MultiplyVector(clipPlane.forward)
+            * dot;
+
+        float camSpaceDst =
+            -Vector3.Dot(
+                camSpacePos,
+                camSpaceNormal
+            )
+            + nearClipOffset;
+
+        if (Mathf.Abs(camSpaceDst) > nearClipLimit)
+        {
+            Vector4 clipPlaneCameraSpace =
+                new Vector4(
+                    camSpaceNormal.x,
+                    camSpaceNormal.y,
+                    camSpaceNormal.z,
+                    camSpaceDst
+                );
+
+            portalCam.projectionMatrix =
+                xrCamera.CalculateObliqueMatrix(
+                    clipPlaneCameraSpace
+                );
+        }
+        else
+        {
+            portalCam.projectionMatrix =
+                xrCamera.projectionMatrix;
+        }
+    }
+
+    void HandleTravellers()
+    {
+        for (int i = 0; i < trackedTravellers.Count; i++)
+        {
+            PortalTraveller traveller =
+                trackedTravellers[i];
+
+            Transform travellerT =
+                traveller.transform;
+
+            Matrix4x4 m =
+                linkedPortal.transform.localToWorldMatrix *
+                transform.worldToLocalMatrix *
+                travellerT.localToWorldMatrix;
+
+            Vector3 offsetFromPortal =
+                travellerT.position -
+                transform.position;
+
+            int portalSide =
+                System.Math.Sign(
+                    Vector3.Dot(
+                        offsetFromPortal,
+                        transform.forward
+                    )
+                );
+
+            int portalSideOld =
+                System.Math.Sign(
+                    Vector3.Dot(
+                        traveller.previousOffsetFromPortal,
+                        transform.forward
+                    )
+                );
+
+            if (portalSide != portalSideOld)
+            {
+                Vector3 oldPos =
+                    travellerT.position;
+
+                Quaternion oldRot =
+                    travellerT.rotation;
+
+                traveller.Teleport(
+                    transform,
+                    linkedPortal.transform,
+                    m.GetColumn(3),
+                    m.rotation
+                );
+
+                traveller.graphicsClone.transform
+                    .SetPositionAndRotation(
+                        oldPos,
+                        oldRot
+                    );
+
+                linkedPortal.OnTravellerEnterPortal(
+                    traveller
+                );
+
+                trackedTravellers.RemoveAt(i);
+                i--;
+            }
+            else
+            {
+                traveller.graphicsClone.transform
+                    .SetPositionAndRotation(
+                        m.GetColumn(3),
+                        m.rotation
+                    );
+
+                traveller.previousOffsetFromPortal =
+                    offsetFromPortal;
+            }
+        }
+    }
+
+    void OnTravellerEnterPortal(
+        PortalTraveller traveller)
+    {
+        if (!trackedTravellers.Contains(traveller))
+        {
+            traveller.EnterPortalThreshold();
+
+            traveller.previousOffsetFromPortal =
+                traveller.transform.position -
+                transform.position;
+
+            trackedTravellers.Add(traveller);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        PortalTraveller traveller =
+            other.GetComponent<PortalTraveller>();
+
+        if (traveller)
+        {
+            OnTravellerEnterPortal(traveller);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        PortalTraveller traveller =
+            other.GetComponent<PortalTraveller>();
+
+        if (traveller &&
+            trackedTravellers.Contains(traveller))
+        {
+            traveller.ExitPortalThreshold();
+            trackedTravellers.Remove(traveller);
+        }
+    }
+
+    public void PrePortalRender()
+    {
+        // Compatibility stub for original MainCamera.cs
+    }
+
+    public void PostPortalRender()
+    {
+        // Compatibility stub for original MainCamera.cs
+    }
+}
